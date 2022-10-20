0547b88f8c7a609b1f18643f000c5f8aafaa20d3
IlianCode <benaissailian@outlook.fr>
2022-10-20 15:43:42 +0200

    jeu final : add case if dice != 6

--- jeu/jeufinal.c
+++ jeu/jeufinal.c
@@ -15,6 +15,7 @@ int rollDice();
 int recherche(int x, int (*pGameBoard)[2][56], int player, int horse, int nbPlayer);
 void changeCase(int x, int (*pGameBoard)[2][56], int player, int horse, int caseToChange);
 int isEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, int nbPlayer);
+int getIndiceNotEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, int nbPlayer, int * checkEmpty);
 //================================================================================================
 
 int main(int argc, char* argv[]){
@@ -23,7 +24,7 @@ int main(int argc, char* argv[]){
     //array of pid
     int pidTab[n];
 
-
+    int checkEmpty[3];
     //variable du jeu 
     int nextPlayer = 0;
     bool diceIsSix = false;
@@ -131,14 +132,95 @@ int main(int argc, char* argv[]){
         }
         printf("Main process read x : %d from pipe %d\n", dice, n);
 
-       /* if(dice!=6){
-                //if no horse 0 but horse 1 on the gameboard, horse 1 moves
+        if(dice!=6){
+            //if no horse 0 but horse 1 on the gameboard, horse 1 moves
+            if(recherche(1, pGameBoard,nextPlayer,0,n) ==-1){
+                if(recherche(1, pGameBoard,nextPlayer,1,n) !=-1){
+                    int indiceCheval = recherche(1, pGameBoard,nextPlayer,1,n);
+                    int checkIfEmpty = isEmpty(pGameBoard, nextPlayer, 1, indiceCheval+dice, n);
 
+                    if(checkIfEmpty != -1){
+                        changeCase(0, pGameBoard, nextPlayer, 1, indiceCheval);
+                        changeCase(1, pGameBoard, nextPlayer, 1, indiceCheval+dice);
+                    }
+                    else{
+                        getIndiceNotEmpty(pGameBoard, nextPlayer, 1, indiceCheval+dice, n, checkEmpty);
 
+                        changeCase(0, pGameBoard, checkEmpty[0], checkEmpty[1], checkEmpty[2]);
+                        changeCase(1, pGameBoard, nextPlayer, 1, indiceCheval+dice);
+                        printf("The horse %d of player %d is no longer on the gameboard\n", checkEmpty[1], checkEmpty[0]);
 
+                    }
+                    printf("Player %d moved horse 1 from case %d to case %d\n", nextPlayer, indiceCheval, indiceCheval+dice);
+                }
+            }
+            //if no horse 1 but horse 0 on the gameboard, horse 0 move
 
+            else if(recherche(1, pGameBoard,nextPlayer,0,n) != -1){
+                if(recherche(1, pGameBoard,nextPlayer,1,n) ==-1){
 
+                    int indiceCheval = recherche(1,pGameBoard,nextPlayer,0,n);
+                    int checkIfEmpty = isEmpty(pGameBoard, nextPlayer, 0, indiceCheval+dice, n);
 
+                    if(checkIfEmpty != -1){
+                        changeCase(0, pGameBoard, nextPlayer, 0, indiceCheval);
+                        changeCase(1, pGameBoard, nextPlayer, 0, indiceCheval+dice);
+                    }
+                    else{
+                        getIndiceNotEmpty(pGameBoard, nextPlayer, 0, indiceCheval+dice, n, checkEmpty);
+
+                        changeCase(0, pGameBoard, checkEmpty[0], checkEmpty[1], checkEmpty[2]);
+                        changeCase(1, pGameBoard, nextPlayer, 0, indiceCheval+dice);
+                        printf("The horse %d of player %d is no longer on the gameboard\n", checkEmpty[1], checkEmpty[0]);
+                    }
+                    caseMoved = indiceCheval+dice;
+                    printf("Player %d has moved his horse 0 from case %d to case %d\n", nextPlayer, indiceCheval, indiceCheval+dice);
+                }
+            }
+            //if two horse on the gameboard, ask which one to move 
+            else if((recherche(1, pGameBoard,nextPlayer,0,n) != -1) && (recherche(1, pGameBoard,nextPlayer,1,n) != -1)){
+                printf("Which horse do you want to move ? (0 or 1)\n");
+                int horse ;
+                scanf("%d", &horse);
+                if(horse ==0){
+                    int indiceCheval = recherche(1,pGameBoard,nextPlayer,0,n);
+                    int checkIfEmpty = isEmpty(pGameBoard, nextPlayer, 0, indiceCheval+dice, n);
+
+                    if(checkIfEmpty != -1){
+                        changeCase(0, pGameBoard, nextPlayer, 0, indiceCheval);
+                        changeCase(1, pGameBoard, nextPlayer, 0, indiceCheval+dice);
+                    }
+                    else{
+                        getIndiceNotEmpty(pGameBoard, nextPlayer, 0, indiceCheval+dice, n, checkEmpty);
+
+                        changeCase(0, pGameBoard, checkEmpty[0], checkEmpty[1], checkEmpty[2]);
+                        changeCase(1, pGameBoard, nextPlayer, 0, indiceCheval+dice);
+                        printf("The horse %d of player %d is no longer on the gameboard\n", checkEmpty[1], checkEmpty[0]);
+                    }
+                    caseMoved = indiceCheval+dice;
+                    printf("Player %d has moved his horse 0 from case %d to case %d\n", nextPlayer, indiceCheval, indiceCheval+dice);
+
+                }else if(horse == 1){
+                    int indiceCheval = recherche(1, pGameBoard,nextPlayer,1,n);
+                    int checkIfEmpty = isEmpty(pGameBoard, nextPlayer, 1, indiceCheval+dice, n);
+
+                    if(checkIfEmpty != -1){
+                        changeCase(0, pGameBoard, nextPlayer, 1, indiceCheval);
+                        changeCase(1, pGameBoard, nextPlayer, 1, indiceCheval+dice);
+                    }
+                    else{
+                        getIndiceNotEmpty(pGameBoard, nextPlayer, 1, indiceCheval+dice, n, checkEmpty);
+
+                        changeCase(0, pGameBoard, checkEmpty[0], checkEmpty[1], checkEmpty[2]);
+                        changeCase(1, pGameBoard, nextPlayer, 1, indiceCheval+dice);
+                        printf("The horse %d of player %d is no longer on the gameboard\n", checkEmpty[1], checkEmpty[0]);
+
+                    }
+                    caseMoved = indiceCheval+dice;
+                    printf("Player %d moved horse 1 from case %d to case %d\n", nextPlayer, indiceCheval, caseMoved);
+                }
+                }
+            }
 
             //if the lastplayer to play was n-1, the next player is 0
             if(nextPlayer == n-1){
@@ -149,7 +231,9 @@ int main(int argc, char* argv[]){
                 nextPlayer++;
             }
 
-        }*/
+        }else {
+
+        }
 
         //finishing close all pipes
         close(pipes[0][0]);
@@ -166,9 +250,10 @@ int main(int argc, char* argv[]){
         }
 
     }
+    changeCase(1, pGameBoard, 1, 0, 47);
+    printf("%d ", recherche(1, pGameBoard, 0, 0, n));
 
-
-    //print gameboard
+    //print f gameboard
     for(i=0; i<n; i++){
         for(j=0; j<2; j++){
             for(k=0; k<56; k++){
@@ -178,19 +263,11 @@ int main(int argc, char* argv[]){
         }
         printf("\n");
     }
-    changeCase(1, pGameBoard, 0, 1, 29);
-    //print gameboard
-    printf("====================\n");
-    for(i=0; i<n; i++){
-        for(j=0; j<2; j++){
-            for(k=0; k<56; k++){
-                printf("%d ", gameBoard[i][j][k]);
-            }
-            printf("\n");
-        }
-        printf("\n");
+    if(!isEmpty(pGameBoard, 0, 1, 33, n)){
+        getIndiceNotEmpty(pGameBoard, 0, 1, 33, n, checkEmpty);
+        printf("\ncheckEmpty[0] : %d, checkEmpty[1] : %d, checkEmpty[2] : %d", checkEmpty[0], checkEmpty[1], checkEmpty[2]);
     }
-    printf("%d", isEmpty(pGameBoard, 0, 0, 29, n));
+
     
     return 0;
 }
@@ -204,14 +281,16 @@ int rollDice(){
     int d1=Random(6) ;
     return d1;
 }
-//if there is a horse in [player][horse] array, return his indice, else return -1
+
+
+//if there is a horse in [player][horse] array, return his indice if yes, else return -1
 int recherche(int x, int (*pGameBoard)[2][56], int player, int horse, int nbPlayer){
     int i ,j ,k;
     int look = x;
     for ( i =0; i<nbPlayer; i++) { 
         for ( j =0; j <2; j++) {
             for ( k =0; k<56; k++){
-                
+                    //for each case of the gameboard we check if there is the value = look -> if there is a horse 
                    if(*(*(*(pGameBoard +i) + j) +k) == look){
                           return k;
                    }
@@ -219,15 +298,20 @@ int recherche(int x, int (*pGameBoard)[2][56], int player, int horse, int nbPlay
             }
          }
     } 
+    //if no horse found, return -1
     return -1;
 }
-//change the case of the horse in [player][horse][caseToChange] array to x
+
+
+
+//change the case of the horse in [player][horse][caseToChange] array to x -> used to make the horse move in the gameBoard
 void changeCase(int x, int (*pGameBoard)[2][56], int player, int horse, int caseToChange){
+    //the function take the int to put in the array, the array, and the position in the array( player, horse, caseToChange) in parameters
     int i ,j ,k;
-    int look = x;
     i=player;
     j=horse;
     k=caseToChange;
+    //the next line mean -> in the array case pGameBoard[i][j][k] put the value x
      *(*(*(pGameBoard +i) + j) +k) = x;
       
 }
@@ -238,7 +322,8 @@ int isEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, in
     i = player;
     j = horse;
     k = caseToCheck;
-    bool quatorze = false;
+    //for each player at the same place in the gameboard check if there is a horse
+    //check if the second horse of the player is here = no change, check if the first horse of next player is here k=+14 
     for(int y=0; y<nbPlayer; y++){
         for(int x =0; x<2; x++){
                 if(*(*(*(pGameBoard +y) + x) +k) != 0){
@@ -250,6 +335,7 @@ int isEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, in
             if (x==1){
                 k+=14;
             }
+            //if k > size of array (56) > k-=56 (go back to the beginning of the array)
             if(k>55){
                 k-=56;
 
@@ -258,4 +344,38 @@ int isEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, in
         
     }
     return 1;
+}
+
+int getIndiceNotEmpty(int (*pGameBoard)[2][56], int player, int horse, int caseToCheck, int nbPlayer, int * checkEmpty){
+    int i ,j ,k;
+    i = player;
+    j = horse;
+    k = caseToCheck;
+    for(int y=0; y<nbPlayer; y++){
+        for(int x =0; x<2; x++){
+                if(*(*(*(pGameBoard +y) + x) +k) != 0){
+                    printf("\n case non vide :  player : %d, horse : %d, case : %d\n", y, x, k);
+                    checkEmpty[0] = y;
+                    checkEmpty[1] = x;
+                    checkEmpty[2] = k;
+                    return checkEmpty[3];
+                }
+            
+            printf("[%d][%d][%d] = %d ", y, x, k, *(*(*(pGameBoard +y) + x) +k));
+            if (x==1){
+                k+=14;
+            }
+            //if k > size of array (56) > k-=56 (go back to the beginning of the array)
+            if(k>55){
+                k-=56;
+
+            }   
+        }
+        
+    }
+    checkEmpty[0] = -1;
+    checkEmpty[1] = -1;
+    checkEmpty[2] = -1;
+    return checkEmpty[3];
+
 }
